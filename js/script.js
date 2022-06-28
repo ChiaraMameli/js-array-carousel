44314af0ce562586f305e21b5d3c48e284a2a1e4
ChiaraMameli <mameli.chiara@libero.it>
2022-06-28 14:36:58 +0200

    completed

--- js/script.js
+++ js/script.js
@@ -1,32 +1,46 @@
-/*
-Creato un array contenente una lista di cinque immagini tra quelle fornite, 
-creare un carosello ispirandoci alle foto in allegato.
+// Genero le immagini da JS
+const sources = ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'];
 
-MILESTONE 1
-Per prima cosa, creiamo il markup statico: 
-costruiamo il container e inseriamo l'immagine grande in modo da poter stilare lo slider; 
-avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
+let imageFile = '';
 
-MILESTONE 2
-Adesso rimuoviamo tutto il markup statico e inseriamo le immagini dinamicamente 
-servendoci dell'array fornito e un semplice ciclo for. 
-Possiamo concatenare una stringa con un template literal oppure utilizzare gli altri metodi di manipolazione del DOM che abbiamo visto insieme. 
-Al termine di questa fase ci ritroveremo con lo stesso slider, 
-ma costruito dinamicamente attraverso JavaScript.
+for(let i = 0; i < sources.length; i++){
+    imageFile += `<img src="${sources[i]}" alt="landscape${i + 1}"/>`
+}
 
-MILESTONE 3
-Al click dell'utente sulle frecce, l'immagine attiva cambia e diventa visibile nello slider, 
-prendendo il posto della precedente.
+const gallery = document.querySelector('.gallery');
 
-BONUS 1:
-Aggiungere il ciclo infinito del carosello. 
-Ovvero se l' immagine attiva è la prima e l'utente clicca la freccia per andare indietro, 
-la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura 
-se l'utente clicca la freccia verso avanti, deve attivarsi la prima immgine.
+gallery.innerHTML = imageFile;
 
-BONUS 2:
-Creiamo delle miniature di tutte le immagni, in cui dovrà apparire in evidenza l’immagine equivalente a quella attiva, 
-scegliete liberamente se scurire le altre immagini oppure se evidenziarla semplicemente con un bordo. 
-Tra queste miniature, quella corrispondente all'immagine attiva deve evidenziarsi, scegliete voi l'effetto estetico, 
-potete colorarla diversamente rispetto alle altre o aggiungere un semplice bordo.
-*/
+// Aggancio la classe active
+const images = document.querySelectorAll('#carousel img');
+
+let currentActiveIndex = 0;
+
+images[currentActiveIndex].classList.add('active');
+
+// Collego i bottoni
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+//Bottone next
+nextButton.addEventListener('click', function(){
+    images[currentActiveIndex].classList.remove('active');
+    currentActiveIndex++;
+
+    if(currentActiveIndex === images.length) {
+        currentActiveIndex = 0;
+    }
+    images[currentActiveIndex].classList.add('active');
+})
+
+// Bottone prev
+
+prevButton.addEventListener('click', function(){
+    images[currentActiveIndex].classList.remove('active');
+    currentActiveIndex--;
+
+    if(currentActiveIndex < 0) {
+        currentActiveIndex = images.length - 1;
+    }
+    images[currentActiveIndex].classList.add('active');
+})
